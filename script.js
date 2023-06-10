934bacaed6806c5e8cc62c178f75a2da1f76ddad
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-10 17:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const p = document.getElementById("counter");
+const btn = document.getElementById("incrementBtn");
+btn.addEventListener("click",()=>{
+	alert(p.innerText);
+	p.innerText=Number(p.innerText)+1;
+})
